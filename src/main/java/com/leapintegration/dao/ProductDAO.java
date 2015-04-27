fabfbc27124e5939fc4579205a6908a4597b71f8
tommy <tommitomov@gmail.com>
2015-04-27 22:42:30 +0300

    Added product DAO

---
+++ src/main/java/com/leapintegration/dao/ProductDAO.java
@@ -0,0 +1,20 @@
+package com.leapintegration.dao;
+
+import com.leapintegration.model.ProductModel;
+
+import java.util.List;
+
+/**
+ * Created by Tommy on 4/27/2015.
+ */
+public interface ProductDAO {
+    //add, remove, search, edit
+
+    ProductModel addProduct(ProductModel productModel);
+    boolean removeProduct(int id);
+    ProductModel editProduct(int id);
+    List<ProductModel> getAllProducts();
+
+
+
+}
