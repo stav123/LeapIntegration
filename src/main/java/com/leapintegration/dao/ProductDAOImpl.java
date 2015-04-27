66c71181e339f24888963d68be46a2534186d609
StefanS <stefanstavrakev@gmail.com>
2015-04-27 22:17:16 +0300

    added spring configs + dummy service impl + dao impls

---
+++ src/main/java/com/leapintegration/dao/ProductDAOImpl.java
@@ -0,0 +1,9 @@
+package com.leapintegration.dao;
+
+/**
+ * Created by StefanS on 4/27/15.
+ */
+public class ProductDAOImpl {
+
+
+}
---
+++ src/main/java/com/leapintegration/service/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package com.leapintegration.service;
+
+/**
+ * Created by StefanS on 4/27/15.
+ */
+public class ProductServiceImpl {
+}
